8e78eb6afc615596e42638f8aa527fce30fe268e
FlaBBB <f12345ff67@gmail.com>
2023-06-23 23:17:27 +0700

    fix base64 encoding

---
+++ challenge/base64.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static char DICT[] = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+
+char *base64_encode(char input[], size_t input_length, size_t *output_length)
+{
+    int pad, temp, last_in, j;
+    last_in = 0;
+    pad = 0;
+    *output_length = ((input_length + 2) / 3) * 4 + 1;
+    char *result = malloc(*output_length);
+    if (result == NULL)
+    {
+        printf("Failed to allocate memory for result.\n");
+        return NULL; // Or handle the error in an appropriate way
+    }
+    j = 0;
+    for (int i = 0; i < input_length; i++)
+    {
+        temp = 0;
+        if (last_in != 0)
+        {
+            temp += ((last_in) & ((3 << (2 * (pad - 1))) | 3)) << (6 - (2 * pad));
+        }
+        pad++;
+        temp += (int)input[i] >> (2 * pad);
+        result[j++] = DICT[temp];
+        if (pad == 3)
+        {
+            result[j++] = DICT[(int)input[i] & 63];
+            last_in = 0;
+            pad = 0;
+        }
+        else
+        {
+            last_in = (int)input[i];
+        }
+    }
+
+    if (last_in != 0)
+    {
+        temp = (last_in & ((3 << (2 * (pad - 1))) | 3)) << (6 - (2 * pad));
+        result[j++] = DICT[temp];
+        result[j++] = '=';
+        if (pad == 1)
+        {
+            result[j++] = '=';
+        }
+    }
+    result[*output_length - 1] = '\0';
+    return result;
+}
+
+int main()
+{
+    size_t output_length = 0;
+    char input[101];
+    printf("input: ");
+    scanf("%100s", input);
+    char *encoded = base64_encode(input, strlen(input), &output_length);
+    printf("encoded: %s", encoded);
+    free(encoded);
+    return 0;
+}
